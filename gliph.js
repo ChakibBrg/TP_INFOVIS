a253b8c81d9e2d00f0fb5023db15674c3bfd73e2
Mohammed Tirichine <mohtiri718@gmail.com>
2024-01-13 16:46:17 +0100

    Implemented stacked bar chart

--- gliph.js
+++ gliph.js
@@ -3,113 +3,139 @@ import raw from './ressources/data.json' assert { type: 'json' };
 
 const data = raw['data'];
 
-console.log(data);
-
 // Variables à utiliser pour la visualisation
 const variables = ['sexe', 'wilaya', 'age', 'niveau_etude'];
 
-// Création de la visualisation avec un diagramme à barres
-const svg = d3.select('#visualization');
-
-// Fonction pour préparer les données à visualiser
-const prepareData = () => {
-	const preparedData = {};
-
-	variables.forEach((variable) => {
-		preparedData[variable] = d3.rollup(
-			data,
-			(v) => v.length,
-			(d) => d[variable]
-		);
-	});
-
-	return preparedData;
-};
-
 // Création du diagramme à barres
 const renderBarChart = () => {
-	const axeHeight = 550;
-	const barsWidth = 450;
+	// Define age intervals
+	const maxAge = Math.max(...data.map((d) => d.age));
+	const minAge = Math.min(...data.map((d) => d.age));
+	const ageIntervals = Array.from(
+		{ length: Math.ceil((maxAge - minAge) / 10) + 1 },
+		(_, i) => minAge - (minAge % 10) + (i + 1) * 10
+	);
 
-	const preparedData = prepareData();
-	const chosenVar = preparedData[variables[3]];
+	data.sort((a, b) => {
+		if (a.age == b.age) return 0;
+		return a.age < b.age ? -1 : 1;
+	});
+
+	// Categorize individuals into age groups
+	const ageGroups = d3.rollup(
+		// Remove individuals with no disease
+		data.filter((d) => d.maladie),
+		(v) => v.length,
+		(d) => {
+			const ageInterval = ageIntervals.find((interval) => d.age <= interval);
+			return ageInterval
+				? ageInterval == ageIntervals[0]
+					? `${ageInterval} and less`
+					: `${ageInterval - 9}-${ageInterval}`
+				: `${ageIntervals[ageIntervals.length - 1]}+`;
+		},
+		(d) => d.maladie
+	);
+
+	// Organize data for d3 stack
+	const ageGroupsMap = [...ageGroups.keys()].map((k) => {
+		const diseasesMap = new Map(ageGroups.get(k));
+		diseasesMap.set('age', k);
+		return Object.fromEntries(diseasesMap.entries());
+	});
+
+	const ages = [...ageGroups.keys()];
+	const maladies = [...new Set(data.map((d) => d.maladie))].filter((d) => d);
+	const maxPersonsPerGroup = Math.max(
+		...[...ageGroups.values()].map((d) =>
+			[...d.values()].reduce((a, b) => a + b)
+		)
+	);
+
+	var margin = { top: 10, right: 30, bottom: 20, left: 50 },
+		width = 460 - margin.left - margin.right,
+		height = 400 - margin.top - margin.bottom;
+
+	const svg = d3
+		.select('#bar-chart')
+		.append('svg')
+		.attr('width', width + margin.left + margin.right)
+		.attr('height', height + margin.top + margin.bottom)
+		.append('g')
+		.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
 	// Définir les échelles pour les axes X et Y
-	const xScale = d3
-		.scaleBand()
-		.domain([...chosenVar.keys()])
-		.range([50, barsWidth])
-		.padding(0.5);
-
-	const yScale = d3
-		.scaleLinear()
-		.domain([0, d3.max([...chosenVar.values()])])
-		.range([axeHeight, 50]);
-
-	// Ajouter les barres au diagramme
+	// Add X axis
+	const x = d3.scaleBand().domain(ages).range([0, width]).padding([0.2]);
 	svg
+		.append('g')
+		.attr('transform', `translate(0, ${height})`)
+		.call(d3.axisBottom(x).tickSizeOuter(0));
+
+	// Add Y axis
+	const y = d3
+		.scaleLinear()
+		.domain([0, maxPersonsPerGroup])
+		.range([height, 0]);
+	svg.append('g').call(d3.axisLeft(y));
+
+	const color = d3.scaleOrdinal().domain(maladies).range(d3.schemeSet2);
+
+	const stackedData = d3.stack().keys(maladies)(ageGroupsMap);
+
+	svg
+		.append('g')
+		.selectAll('g')
+		// Enter in the stack data = loop key per key = group per group
+		.data(stackedData)
+		.join('g')
+		.attr('fill', (d) => color(d.key))
+		.attr('class', (d) => 'myRect ' + d.key.replaceAll(' ', '-')) // Add a class to each subgroup: their name
 		.selectAll('rect')
-		.data([...chosenVar.entries()])
-		.enter()
-		.append('rect')
-		.attr('x', (d) => xScale(d[0]))
-		.attr('y', (d) => yScale(d[1]))
-		.attr('width', xScale.bandwidth())
-		.attr('height', (d) => axeHeight - yScale(d[1]))
-		.attr('fill', 'steelblue')
+		// enter a second time = loop subgroup per subgroup to add all rectangles
+		.data((d) => d)
+		.join('rect')
+		.attr('x', (d) => x(d.data.age))
+		.attr('y', (d) => y(d[1]))
+		.attr('height', (d) => {
+			const h = y(d[0]) - y(d[1]);
+			return isNaN(h) ? 0 : h;
+		})
+		.attr('width', x.bandwidth())
+		.attr('stroke', 'grey')
 		.on('mouseover', handleMouseOver)
-		.on('mouseout', handleMouseOut)
+		.on('mouseleave', handleMouseOut)
 		.on('mousemove', handleMouseMove);
 
-	// Ajouter les labels des barres
-	svg
-		.selectAll('text')
-		.data([...chosenVar.entries()])
-		.enter()
-		.append('text')
-		.attr('x', (d) => xScale(d[0]) + xScale.bandwidth() / 2)
-		.attr('y', (d) => yScale(d[1]) - 5)
-		.attr('text-anchor', 'middle')
-		.text((d) => d[1])
-		.attr('font-family', 'sans-serif')
-		.attr('font-size', '12px')
-		.attr('fill', 'black');
-
 	// Tooltip for hover interactions
-	const tooltip = d3.select('body').append('div').attr('class', 'tooltip');
+	const tooltip = d3
+		.select('#bar-chart')
+		.append('div')
+		.attr('class', 'tooltip');
 
 	function handleMouseOver(event, d) {
-		d3.select(this).attr('fill', 'orange');
+		const maladie = d3.select(this.parentNode).datum().key;
+		d3.selectAll('.myRect').style('opacity', 0.2);
+		d3.selectAll('.' + maladie.replaceAll(' ', '-')).style('opacity', 1);
+
+		var maladieValue = d.data[maladie];
 
 		tooltip
 			.style('display', 'block')
-			.html(`<p>${d[0]}: ${d[1]}</p>`)
-			.style('left', `${event.pageX}px`)
-			.style('top', `${event.pageY}px`);
+			.html(`<p>${maladie}: ${maladieValue}</p>`);
 	}
 
 	function handleMouseOut() {
-		d3.select(this).attr('fill', 'steelblue');
+		d3.selectAll('.myRect').style('opacity', 1);
 
 		tooltip.style('display', 'none');
 	}
 
 	function handleMouseMove(event) {
-		tooltip.style('left', `${event.pageX + 10}px`).style('top', `${event.pageY + 10}px`);
+		tooltip
+			.style('left', `${event.pageX + 10}px`)
+			.style('top', `${event.pageY + 10}px`);
 	}
-
-	// Ajouter l'axe X
-	svg
-		.append('g')
-		.attr('transform', `translate(0,${axeHeight})`)
-		.call(d3.axisBottom(xScale));
-
-	// Ajouter l'axe Y
-	svg
-		.append('g')
-		.attr('transform', 'translate(50,0)')
-		.call(d3.axisLeft(yScale));
 };
 
-// Appel de la fonction pour créer le diagramme à barres
 renderBarChart();
