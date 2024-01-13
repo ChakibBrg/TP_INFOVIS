e366b6a4ef98ba50ea838dcd3ed195c5064050df
Mohammed Tirichine <mohtiri718@gmail.com>
2024-01-13 19:26:17 +0100

    Implemented Map chart

--- gliph.js
+++ gliph.js
@@ -1,10 +1,26 @@
 // Données fictives pour simuler la base de données
 import raw from './ressources/data.json' assert { type: 'json' };
+import dz from './ressources/algeria@7.json' assert { type: 'json' };
 
-const data = raw['data'];
+const data = raw.data;
+const maladies = [
+	...new Set(
+		data
+			.sort((a, b) => {
+				if (a.age == b.age) return 0;
+				return a.age < b.age ? -1 : 1;
+			})
+			.map((d) => d.maladie)
+	),
+].filter((d) => d);
+
+// const wilayas = dz.objects.DZA_adm1.geometries.map((d) => d.properties.NAME_1);
 
 // Variables à utiliser pour la visualisation
 const variables = ['sexe', 'wilaya', 'age', 'niveau_etude'];
+
+// Tooltip for hover interactions
+const tooltip = d3.select('body').append('div').attr('class', 'tooltip');
 
 // Création du diagramme à barres
 const renderBarChart = () => {
@@ -15,11 +31,6 @@ const renderBarChart = () => {
 		{ length: Math.ceil((maxAge - minAge) / 10) + 1 },
 		(_, i) => minAge - (minAge % 10) + (i + 1) * 10
 	);
-
-	data.sort((a, b) => {
-		if (a.age == b.age) return 0;
-		return a.age < b.age ? -1 : 1;
-	});
 
 	// Categorize individuals into age groups
 	const ageGroups = d3.rollup(
@@ -45,7 +56,6 @@ const renderBarChart = () => {
 	});
 
 	const ages = [...ageGroups.keys()];
-	const maladies = [...new Set(data.map((d) => d.maladie))].filter((d) => d);
 	const maxPersonsPerGroup = Math.max(
 		...[...ageGroups.values()].map((d) =>
 			[...d.values()].reduce((a, b) => a + b)
@@ -73,10 +83,7 @@ const renderBarChart = () => {
 		.call(d3.axisBottom(x).tickSizeOuter(0));
 
 	// Add Y axis
-	const y = d3
-		.scaleLinear()
-		.domain([0, maxPersonsPerGroup])
-		.range([height, 0]);
+	const y = d3.scaleLinear().domain([0, maxPersonsPerGroup]).range([height, 0]);
 	svg.append('g').call(d3.axisLeft(y));
 
 	const color = d3.scaleOrdinal().domain(maladies).range(d3.schemeSet2);
@@ -104,14 +111,8 @@ const renderBarChart = () => {
 		.attr('width', x.bandwidth())
 		.attr('stroke', 'grey')
 		.on('mouseover', handleMouseOver)
-		.on('mouseleave', handleMouseOut)
+		.on('mouseleave', handleMouseLeave)
 		.on('mousemove', handleMouseMove);
-
-	// Tooltip for hover interactions
-	const tooltip = d3
-		.select('#bar-chart')
-		.append('div')
-		.attr('class', 'tooltip');
 
 	function handleMouseOver(event, d) {
 		const maladie = d3.select(this.parentNode).datum().key;
@@ -125,7 +126,7 @@ const renderBarChart = () => {
 			.html(`<p>${maladie}: ${maladieValue}</p>`);
 	}
 
-	function handleMouseOut() {
+	function handleMouseLeave() {
 		d3.selectAll('.myRect').style('opacity', 1);
 
 		tooltip.style('display', 'none');
@@ -138,4 +139,97 @@ const renderBarChart = () => {
 	}
 };
 
+const renderMapChart = () => {
+	const dataWithMaladie = data.filter((d) => d.maladie);
+	const wilayasMap = d3.rollup(
+		dataWithMaladie,
+		(v) => v.length,
+		(d) => d.wilaya
+	);
+	const wilayasMaladiesMap = d3.rollup(
+		dataWithMaladie,
+		(v) => v.length,
+		(d) => d.wilaya,
+		(d) => d.maladie
+	);
+
+	const colorScale = d3
+		.scaleThreshold()
+		.domain([0, ...new Set([...wilayasMap.values()].sort((a, b) => a - b))])
+		.range(d3.schemeReds[9]);
+
+	const width = 460;
+	const height = 400;
+
+	const svg = d3
+		.select('#map-chart')
+		.append('svg')
+		.attr('viewBox', `${-width / 2} ${-height / 2} ${width * 2} ${height * 2}`)
+		.attr('transform', `scale(-1, 1) rotate(180)`);
+
+	const g = svg.append('g');
+
+	const path = d3.geoPath();
+
+	g.append('g')
+		.attr('cursor', 'pointer')
+		.selectAll('path')
+		.data(topojson.feature(dz, dz.objects.DZA_adm1).features)
+		.join('path')
+		.attr('stroke', '#999')
+		.attr('stroke-width', 0.4)
+		.attr('d', path)
+		.attr('fill', (d) => {
+			const wilaya = d.properties.NAME_1;
+			const maladie = wilayasMap.get(wilaya);
+			return colorScale(maladie) || '#f4f4f4';
+		})
+		.attr('class', (d) => 'wilaya ' + d.properties.NAME_1.replaceAll(' ', '-').replaceAll("'", ''))
+		.on('mouseover', handleMouseOver)
+		.on('mouseleave', handleMouseLeave)
+		.on('mousemove', handleMouseMove);
+		// .append('title')
+		// .text((d) => d.properties.NAME_1);
+
+	g.append('path')
+		.attr('fill', 'none')
+		.attr('stroke-linejoin', 'round')
+		.attr('d', path(topojson.mesh(dz, dz.objects.DZA_adm1, (a, b) => a !== b)));
+
+	function handleMouseOver(event, d) {
+		const wilaya = d.properties.NAME_1;
+		d3.selectAll('.wilaya').style('opacity', 0.2);
+		d3.selectAll('.' + wilaya.replaceAll(' ', '-').replaceAll("'", ''))
+			.style('opacity', 1)
+			.style('stroke', 'black');
+		const wilayaCount = wilayasMap.get(wilaya) || 0;
+
+		tooltip.style('display', 'block')
+			.html(`<h3>${wilaya}</h3><ol>${
+			wilayasMaladiesMap.get(wilaya)
+				? [...wilayasMaladiesMap.get(wilaya).entries()]
+						.sort((a, b) => b[1] - a[1])
+						.map((d) => `<li>${d[0]}: ${d[1]}</li>`)
+						.join('')
+				: ''
+		}</ol>
+		`);
+	}
+
+	function handleMouseLeave() {
+		d3.selectAll('.wilaya')
+			.style('opacity', 1)
+			.style('stroke', d3.select(this).attr('stroke'));
+
+		tooltip.style('display', 'none');
+	}
+
+	function handleMouseMove(event) {
+		tooltip
+			.style('left', `${event.pageX + 10}px`)
+			.style('top', `${event.pageY + 10}px`);
+	}
+};
+
 renderBarChart();
+renderMapChart();
